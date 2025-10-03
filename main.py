589312f7f2c3e07177f2629c6e2469df8430ecaf
abuzuhair04 <aminkafri0045@gmail.com>
2025-10-03 10:37:48 +0800

    dump dir content using branch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 
-OFFSET_STRING = " "
+MID_BRANCH = "├──"
+LAST_BRANCH = "└──"
+NO_BRANCH = "│"
+
+OFFSET_STRING = ""
 
 def main():
     path = "test_dir"
@@ -8,21 +12,29 @@ def main():
     print(path)
     dump_tree([OFFSET_STRING], path)
 
-def dump_tree(tabs, path):
+def dump_tree(offset_string_list, path):
     try:
-        entries = os.scandir(path)
+        entries = list(os.scandir(path))
     except FileNotFoundError as e:
         print(f"dirtree error: {e}")
         exit(1)
 
-    for entry in entries:
-        if entry.is_dir():
-            print("".join(tabs), "-", entry.name)
-            tabs.append(OFFSET_STRING)
-            dump_tree(tabs, entry.path)
-            tabs.pop()
+    entry_len = len(entries)
+    for i, entry in enumerate(entries):
+        entry_name = ""
+        if i == entry_len - 1:
+            entry_name = f"{LAST_BRANCH} {entry.name}"
         else:
-            print("".join(tabs), "-", entry.name)
+            entry_name = f"{MID_BRANCH} {entry.name}"
+
+
+        if entry.is_dir():
+            print("".join(offset_string_list) + entry_name)
+            offset_string_list.append(f"{NO_BRANCH}   ")
+            dump_tree(offset_string_list, entry.path)
+            offset_string_list.pop()
+        else:
+            print("".join(offset_string_list) + entry_name)
 
 
 
