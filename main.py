167cb4ffaed64645cff687a43a68e702827eded8
abuzuhair04 <aminkafri0045@gmail.com>
2025-10-03 09:57:32 +0800

    visualize the tree using OFFSET_STRING

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import os
 
-def main():
-    path = "."
+OFFSET_STRING = " "
 
+def main():
+    path = "test_dir"
+
+    print(path)
+    dump_tree([OFFSET_STRING], path)
+
+def dump_tree(tabs, path):
     try:
-        dircontent = os.scandir(path)
+        entries = os.scandir(path)
     except FileNotFoundError as e:
         print(f"dirtree error: {e}")
+        exit(1)
 
-    for i in dircontent:
-        print(i.path)
-        print(i.is_dir())
+    for entry in entries:
+        if entry.is_dir():
+            print("".join(tabs), "-", entry.name)
+            tabs.append(OFFSET_STRING)
+            dump_tree(tabs, entry.path)
+            tabs.pop()
+        else:
+            print("".join(tabs), "-", entry.name)
+
 
 
 if __name__ == "__main__":
